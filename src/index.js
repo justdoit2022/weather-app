2461106d993946387a737ad93fe5204840d499ce
justdoit2022 <96680065+justdoit2022@users.noreply.github.com>
2022-01-07 00:06:10 +0000

    added JS for forecast
    1. Removed 'weather-forecast' HTML from HTML file & emptied div. 2. Injected HTML into JS file & created 'displayForecast' function, to create multiple forecast columns for each day.

--- src/index.js
+++ src/index.js
@@ -22,6 +22,37 @@ function formatDate(timestamp) {
   return `Last updated: ${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tues", "Wed"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+
+      <img
+        src="http://openweathermap.org/img/wn/50d@2x.png"
+        class="forecast-icon"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+    </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -101,3 +132,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
+displayForecast();
